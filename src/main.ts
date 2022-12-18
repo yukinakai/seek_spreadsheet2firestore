a3b1ed9cdda324f13176d070c918c158c08de7dd
yukinakai <10libra20.y@gmail.com>
2022-12-18 07:57:21 +0700

    update masters

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,12 @@ function main() {
   const props = PropertiesService.getScriptProperties();
   const [email, key, projectId] = [props.getProperty('CLIENT_EMAIL'), props.getProperty('PRIVATE_KEY')!.replace(/\\n/g, '\n'), props.getProperty('PROJECT_ID')];
   const firestore = FirestoreApp.getFirestore(email, key, projectId);
+  // マスターデータ更新用の配列
+  let areas: any[] = [];
+  let employments: any[] = [];
+  let jobTypes: any[] = [];
+  let ages: any[] = [];
+  let others: any[] = [];
   for (var i=0; i<formattedData?.length; i++) {
     const service: Service = formattedData[i]
     if (service.serviceUid) {
@@ -43,6 +49,25 @@ function main() {
       const rowNum = workSheet?.createTextFinder(service.url!).matchEntireCell(true).findAll()[0].getRow();
       workSheet?.getRange(rowNum!, 1).setValue(serviceUid)
     }
+    areas = areas.concat(service.areaFeatures);
+    employments = employments.concat(service.employmentFeatures);
+    jobTypes = jobTypes.concat(service.jobTypeFeatures);
+    ages = ages.concat(service.ageFeatures);
+    others = others.concat(service.otherFeatures);
   }
-  // TODO: マスタデータのアップデート
+  // マスタデータのアップデート
+  const msts: any[][] = [['areas', areas], ['employments', employments], ['jobTypes', jobTypes], ['ages', ages], ['others', others]];
+  for (var c=0; c<msts.length;c++) {
+    const newMst = msts[c][1].filter((value: string, i: number)=>msts[c][1].indexOf(value)==i);
+    const allDocs = firestore.getDocuments(msts[c][0]);
+    const existedMst = allDocs.map((value: {[key: string]: string})=>value.name.split('/').pop());
+    const createMst = newMst.filter((value: string)=>existedMst.indexOf(value)==-1);
+    const deleteMst = existedMst.filter((value: string)=>newMst.indexOf(value)==-1);
+    for (var i=0; i<createMst.length;i++) {
+      firestore.createDocument(msts[c][0]+'/'+createMst[i], {});
+    }
+    for (var i=0; i<deleteMst.length;i++) {
+      firestore.deleteDocument(msts[c][0]+"/"+deleteMst[i]);
+    }
+  }
 }
